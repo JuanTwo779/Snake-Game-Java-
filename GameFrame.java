26d16baaeae006c8d7f46bed9b985f69880df11a
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-10-13 15:34:33 +1100

    Class set ups

---
+++ GameFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GameFrame(){
+//        GamePanel panel = new GamePanel();
+//        this.add(panel);
+
+        this.add(new GamePanel());
+        this.setTitle("2nake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack(); //makes things fit snugly
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); //puts window in middle
+    }
+
+}
---
+++ GamePanel.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
